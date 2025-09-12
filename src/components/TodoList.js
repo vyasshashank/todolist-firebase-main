04ad962addf893b6f36a36b68d2a78f31e5638d6
SHASHANK VYAS <126417044+vyasshashank@users.noreply.github.com>
2025-09-12 13:28:26 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -119,7 +119,6 @@ const TodoList = () => {
   // Handling drop event to move task between lists or within the same list
   const handleDrop = async (targetListId, priority) => {
     const user = getAuth().currentUser;
-    const { id, listId, ...taskData } = draggedTask;
     if (draggedTask) {
       try {
         if (draggedTask.listId !== targetListId) {
@@ -127,7 +126,7 @@ const TodoList = () => {
           await addDoc(
             collection(db, `users/${user.uid}/todoLists/${targetListId}/tasks`),
             {
-              ...taskData,
+              ...draggedTask,
               priority,
               createdAt: new Date(),
             }
